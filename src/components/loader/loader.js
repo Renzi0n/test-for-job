9525b2e4845b7e4803832e9688a2849b73937567
Renzi0n <36129793+Renzi0n@users.noreply.github.com>
2021-05-17 17:41:37 +0300

    Исправлена ошибка

--- src/components/loader/loader.js
+++ src/components/loader/loader.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Loader = () => {
     return (
-            <div class="loadingio-spinner-spinner-zq7g0epvca">
-                <div class="ldio-t2quo5t3h5c">
+            <div className="loadingio-spinner-spinner-zq7g0epvca">
+                <div className="ldio-t2quo5t3h5c">
                     <div></div>
                     <div></div>
                     <div></div>
